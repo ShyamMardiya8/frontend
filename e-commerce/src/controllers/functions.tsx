fb4cbca0e16cbe4f84a51e97c9ead094b704aa51
ShyamMardiya8 <shyammardiya8@gmail.com>
2025-04-13 12:22:42 +0530

    change url path

--- e-commerce/src/controllers/functions.tsx
+++ e-commerce/src/controllers/functions.tsx
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 
 // const isLocalhost = typeof window !== 'undefined' && window.location.hostname === 'localhost';
 
-const uri = 'https://server-222q.vercel.app/api/'
+const uri = 'https://server-222q.vercel.app/api'
 // 'https://server-88zz-cwzlqp0d4-shyams-projects-a087ff4a.vercel.app/api' 
 // isLocalhost
 //   ? 'http://localhost:3000/api'
